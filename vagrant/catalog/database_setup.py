2ea07355ac55d8300b97ad6b52ebd807a4f52c85
Adnan Jaffal <adnanjaffal@gmail.com>
2016-01-27 20:45:02 -0600

    Practicing with SQLAlchemy commands.

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -1,6 +1,6 @@
 import sys
 
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Date, Float
 
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -10,25 +10,30 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-class Restaurant(Base):
-    __tablename__ = 'restaurant'
+class Shelter(Base):
+    __tablename__ = 'shelter'
+
+    name = Column(String(80), nullable = False)
+    address = Column(String(100), nullable = False)
+    city = Column(String(50), nullable = False)
+    state = Column(String(50), nullable = False)
+    zip_code = Column(Integer, nullable = False)
+    website = Column(String(250))
+    id = Column(Integer, primary_key = True)
+
+class Puppy(Base):
+    __tablename__ = 'puppy'
 
     name = Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
+    date_birth = Column(Date)
+    gender = Column(String(10), nullable = False)
+    weight = Column(Float, nullable = False)
+    shelter_id = Column(Integer, ForeignKey('shelter.id'))
 
-class MenuItem(Base):
-    __tablename__ = 'menu_item'
-
-    name = Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
-    course = Column(String(250))
-    description = Column(String(250))
-    price = Column(String(8))
-    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
-
-    restaurant = relationship(Restaurant)
+    shelter = relationship(Shelter)
     
 
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///puppies.db')
 
 Base.metadata.create_all(engine)
